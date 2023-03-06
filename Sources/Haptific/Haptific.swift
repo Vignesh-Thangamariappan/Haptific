c0c83f7f7fb8279f4ad68f61d212b927aec5da85
Vignesh <vignesh.mariappan@anywhere.co>
2023-03-07 00:51:28 +0530

    Feat: Adds method to simulate Haptic Feedback of different types

--- Sources/Haptific/Haptific.swift
+++ Sources/Haptific/Haptific.swift
@@ -8,9 +8,37 @@
 import Foundation
 import UIKit
 
+public enum HapticType {
+    case impact(style: UIImpactFeedbackGenerator.FeedbackStyle)
+    case selection
+    case notification(style: UINotificationFeedbackGenerator.FeedbackType)
+}
 public class Haptific {
-    static func simulateFeedback() {
-        let feedbackGenerator = UIImpactFeedbackGenerator()
+    private static func simulateImpactFeedback(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let feedbackGenerator = UIImpactFeedbackGenerator(style: style)
+        feedbackGenerator.prepare()
         feedbackGenerator.impactOccurred()
+    }
+    
+    private static func simulateSelectionFeedback() {
+        let selectionFeedbackGenerator = UISelectionFeedbackGenerator()
+        selectionFeedbackGenerator.selectionChanged()
+    }
+    
+    private static func simulateNotficationFeedback(style: UINotificationFeedbackGenerator.FeedbackType) {
+        let notificationFeedbackGenerator = UINotificationFeedbackGenerator()
+        notificationFeedbackGenerator.prepare()
+        notificationFeedbackGenerator.notificationOccurred(style)
+    }
+    
+    public static func simulate(_ type: HapticType) {
+        switch type {
+        case .impact(let style):
+            simulateImpactFeedback(style: style)
+        case .selection:
+            simulateSelectionFeedback()
+        case .notification(let style):
+            simulateNotficationFeedback(style: style)
+        }
     }
 }
